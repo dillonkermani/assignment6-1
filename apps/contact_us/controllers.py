438cdc04222083ed356a3bc0cd8d53fc1a81b255
dillonkermani <60326614+dillonkermani@users.noreply.github.com>
2024-05-14 22:16:59 -0700

    Defined request table

--- apps/contact_us/controllers.py
+++ apps/contact_us/controllers.py
@@ -5,5 +5,18 @@ from py4web.utils.url_signer import URLSigner
 from .models import get_user_email
 from py4web.utils.form import Form, FormStyleBulma
 from py4web.utils.grid import Grid, GridClassStyleBulma
+from py4web import Field, IS_NOT_EMPTY, IS_EMAIL
+import datetime
 
 # Complete. 
+
+db.define_table(
+    'request',
+    Field('name', requires=IS_NOT_EMPTY()),
+    Field('email', requires=IS_EMAIL()),
+    Field('phone', requires=IS_NOT_EMPTY()),
+    Field('message', 'text', requires=IS_NOT_EMPTY()),
+    Field('timestamp', 'datetime', default=lambda: datetime.datetime.utcnow()),
+)
+
+db.commit()
