84264aa133ad88919ff1a2065b7756c9d58a70e6
dillonkermani <60326614+dillonkermani@users.noreply.github.com>
2024-05-15 00:09:30 -0700

    Submit_Request looks functional

--- apps/contact_us/controllers.py
+++ apps/contact_us/controllers.py
@@ -5,8 +5,22 @@ from py4web.utils.url_signer import URLSigner
 from .models import get_user_email
 from py4web.utils.form import Form, FormStyleBulma
 from py4web.utils.grid import Grid, GridClassStyleBulma
-from py4web import Field, IS_NOT_EMPTY, IS_EMAIL
+#from py4web import Field, IS_NOT_EMPTY, IS_EMAIL
 import datetime
 
-# Complete. 
+# Home page action
+@action('index')
+@action.uses('index.html', db, auth.user)
+def index():
+    return dict(
+        submit_request_url = URL('contact_us', 'create_post', signer=URLSigner()),
+    )
 
+# Create a new post
+@action('submit_request', method="POST")
+@action.uses(db, auth.user, session)
+def submit_request():
+    new_request = request.json.get('new_request')
+    request_id = db.post.insert(request=new_request)
+    print("New request created with id: ", request_id)
+    return ((redirect(URL('index'))))
--- apps/contact_us/models.py
+++ apps/contact_us/models.py
@@ -4,22 +4,30 @@ This file defines the database models
 
 import datetime
 from .common import db, Field, auth
-from pydal.validators import *
+
+
+def is_not_empty(value):
+    if value and value.strip():
+        return (value, None)
+    else:
+        return (value, 'Field cannot be empty')
 
 
 def get_user_email():
     return auth.current_user.get('email') if auth.current_user else None
 
+
 def get_time():
     return datetime.datetime.utcnow()
+
 
 # Complete. 
 db.define_table(
     'request',
-    Field('name', requires=IS_NOT_EMPTY()),
-    Field('email', requires=IS_EMAIL()),
-    Field('phone', requires=IS_NOT_EMPTY()),
-    Field('message', 'text', requires=IS_NOT_EMPTY()),
+    Field('name', requires=is_not_empty),
+    Field('email', requires=is_not_empty),
+    Field('phone', requires=is_not_empty),
+    Field('message', 'text', requires=is_not_empty),
     # Field('timestamp', 'datetime', default=lambda: datetime.datetime.utcnow()),
 )
 
